157abff649e26b6cb77d7fc28c4332ad8822aa1f
Agustin Ojeda <agustinojeda20@gmail.com>
2022-12-12 16:50:53 -0300

    Change to upper case

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Homepage() {
   return (
       <div>
-        <h1 className='text-8xl text-[#561baf] font-bold drop-shadow-2xl'>Coming soon</h1>
+        <h1 className='text-8xl text-[#561baf] font-bold drop-shadow-2xl'>COMING SOON!</h1>
       </div>
   )
 }
